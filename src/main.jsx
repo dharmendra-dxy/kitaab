12efe465c7ca25c42dd48840cc84bb0c95c7d72b
Dharmendra Yadav <yaduvanshi.dhanno@gmail.com>
2025-01-17 21:24:26 +0530

    deploy fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { FirebaseProvider } from './context/firebase.jsx'
 import { BrowserRouter } from 'react-router'
+import { FirebaseProvider } from './context/Firebase.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
